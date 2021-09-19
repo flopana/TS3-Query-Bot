e8e09004eacc58cf7a89b5d2ee54d0c4f05aec18
Florian <florian@f-erbs.de>
2021-09-19 14:55:24 +0200

    name change function

---
+++ src/main/java/Functions/NameChangeFunction.java
@@ -0,0 +1,45 @@
+package Functions;
+
+import UserManagement.User;
+import UserManagement.UserManager;
+import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class NameChangeFunction implements FunctionInterface{
+    @Override
+    public void register(TS3Api ts3Api, String path) {
+        Logger logger = LoggerFactory.getLogger(NameChangeFunction.class);
+
+        Thread thread = new Thread(() -> {
+            while (true) {
+                logger.debug("Checking for name changes.");
+
+                List<Client> clients = ts3Api.getClients();
+                UserManager userManager = UserManager.getInstance();
+
+                for(Client client : clients){
+                    User user = userManager.getUser(client.getId());
+
+                    if (!client.getNickname().equals(user.getNickname())){
+                        userManager.removeUser(client.getId());
+                        userManager.addUser(client, userManager.getAdminGroupIds());
+                    }
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.setDaemon(true);
+        logger.info("Starting NameChange function");
+        thread.start();
+    }
+}
