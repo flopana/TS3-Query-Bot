cc03450a27bdff0da27aa694f5b38e1ebae80d26
Florian <florian@f-erbs.de>
2022-02-11 16:56:28 +0100

    instantiate and register routes

---
+++ src/main/java/RestAPI/WebServer.java
@@ -0,0 +1,34 @@
+package RestAPI;
+
+import com.github.theholywaffle.teamspeak3.TS3Api;
+
+import static spark.Spark.*;
+
+public class WebServer {
+    static TS3Api ts3Api;
+
+    public static void startWebserver(TS3Api ts3Api) {
+        setTs3Api(ts3Api);
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        get("/", ((request, response) -> {
+            return "TS3-Query-Bot API";
+        }));
+
+        path("/api", () -> {
+            get("/users", UserController.getUsers);
+            get("/user/:clientId", UserController.getUser);
+
+            get("/channels", ChannelController.getChannels);
+        });
+
+    }
+
+    public static TS3Api getTs3Api() {
+        return ts3Api;
+    }
+
+    public static void setTs3Api(TS3Api ts3Api) {
+        WebServer.ts3Api = ts3Api;
+    }
+}
